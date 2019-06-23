c26dc8e79b874df65c252abac02de557dce3f46d
J Lobos Vega <jalv.vg@gmail.com>
2019-06-23 02:22:28 -0400

    public folder asserts -server.js

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-const express = require["express"];
-const bodyParser = require["body-parser"];
-const exphbs = ["express-handlebars"];
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
 
-const app = express();
-const Port = process.env.PORT || 3306;
+var app = express();
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-const routes = require("./controllers/burger-controllers.js");
+var routes = require("../Hamberder/controllers/burger-controllers.js");
 app.use(routes);
 
-app.Listen(Port , function() {
-    console.log("Server listening on http://localhost:" + PORT);
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
 });
+
